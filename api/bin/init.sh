51a587651c67b0c0636d41460dc952d83724626a
Snugglefuzz <125339664+Snugglefuzz@users.noreply.github.com>
2023-10-28 21:45:47 -0700

    Update init.sh

--- api/bin/init.sh
+++ api/bin/init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# sqlite3 ./var/projectDatabase.db < ./share/projectDatabase.sql
+sqlite3 ./var/primary/fuse/userData.db < ./share/projectDatabase.sql
 sqlite3 ./var/primary/fuse/userData.db < ./share/project2Database.sql
